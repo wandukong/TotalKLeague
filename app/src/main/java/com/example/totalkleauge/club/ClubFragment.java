ee20dfda390359b38f30826ad81482a901cc4102
kym1924 <kym1924@naver.com>
2020-12-05 20:56:39 +0900

    Modify PlanRecyclerView

--- app/src/main/java/com/example/totalkleauge/club/ClubFragment.java
+++ app/src/main/java/com/example/totalkleauge/club/ClubFragment.java
@@ -21,21 +21,21 @@ public class ClubFragment extends Fragment {
 
         return view;
     }
-
+    
     private void setAdapter(RecyclerView rvClub) {
         ClubAdapter clubAdapter = new ClubAdapter();
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "전북 현대\n모터스"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "울산 현대\n축구단"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "포항\n스틸러스"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "상주 상무\n프로축구단"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "대구FC"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "광주FC"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "강원FC"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "수원 삼성\n블루윙즈"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "FC서울"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "성남 FC"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "인천\n유나이티드"));
-        clubAdapter.addClubData(new ClubData(R.drawable.logo, "부산\n아이파크"));
+        clubAdapter.addClubData(new ClubData(R.drawable.jeonbuk, "전북 현대\n모터스"));
+        clubAdapter.addClubData(new ClubData(R.drawable.ulsan, "울산 현대\n축구단"));
+        clubAdapter.addClubData(new ClubData(R.drawable.pohang, "포항\n스틸러스"));
+        clubAdapter.addClubData(new ClubData(R.drawable.sangju, "상주 상무\n프로축구단"));
+        clubAdapter.addClubData(new ClubData(R.drawable.daegu, "대구FC"));
+        clubAdapter.addClubData(new ClubData(R.drawable.gwangju, "광주FC"));
+        clubAdapter.addClubData(new ClubData(R.drawable.gangwon, "강원FC"));
+        clubAdapter.addClubData(new ClubData(R.drawable.suwon, "수원 삼성\n블루윙즈"));
+        clubAdapter.addClubData(new ClubData(R.drawable.seoul, "FC서울"));
+        clubAdapter.addClubData(new ClubData(R.drawable.seongnam, "성남 FC"));
+        clubAdapter.addClubData(new ClubData(R.drawable.incheon, "인천\n유나이티드"));
+        clubAdapter.addClubData(new ClubData(R.drawable.busan, "부산\n아이파크"));
 
         rvClub.setAdapter(clubAdapter);
     }
